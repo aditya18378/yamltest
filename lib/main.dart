a8502b7035a9acb2d2d25be34c438700ced50a15
aditya18378 <44195387+aditya18378@users.noreply.github.com>
2020-03-19 15:54:15 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ void main() {
   File f = new File(pathToYaml);
   String yamlText = f.readAsStringSync();
   // print(yamlText);
-  Map yaml = loadYaml(yamlText);
-  Map _yaml = yaml['dependencies'];
+  Map yaml = loadYaml(yamlText) as Map;
+  Map _yaml = yaml['dependencies'] as Map;
   _yaml.forEach((k, v) =>
       print('$k: $v')); // printing subsets and this is without comments.
 }
